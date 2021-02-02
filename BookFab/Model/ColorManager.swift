04715d811c94555b53fdca072495fbb67e046230
nicolelopezfeliz <nicole.lopez@live.se>
2021-02-02 19:33:45 +0100

    Created Login view

---
+++ BookFab/Model/ColorManager.swift
@@ -0,0 +1,16 @@
+//
+//  ColorManager.swift
+//  BookFab
+//
+//  Created by Nicole Lopez feliz on 2021-02-01.
+//
+
+import SwiftUI
+
+struct ColorManager {
+    //Static variables for my custom colors
+    //I use them by calling: ColorManager.darkPink/.lightPink
+    static let lightPink = Color("LightPink")
+    static let darkPink = Color("DarkPink")
+
+}
